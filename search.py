430c79bc7e203a9acd128f975420e849ecb6ea1c
Manas Acharyya <monteprofilityprod@gmail.com>
2023-03-11 18:13:54 +0530

    UI Component pulling Response from API via ajax request

--- search.py
+++ search.py
@@ -1,8 +1,8 @@
 import json
 
 class Icd10Cm:
-    def __init__(self, code: str, name: str, childLink: str, children):
-        self.code, self.name, self.childLink, self.children = code, name, childLink, children
+    def __init__(self, id: str, name: str, children):
+        self.id, self.name, self.children = id, name, children
     
 def find_node(code):
     with open("dataset.json") as user_file:
@@ -12,13 +12,13 @@ def find_node(code):
 
     for node in parsed_json:
         icd = Icd10Cm(**node)
-        codeStart = icd.code.split("-")[0]
-        codeEnd = icd.code.split("-")[1]
+        codeStart = icd.id.split("-")[0]
+        codeEnd = icd.id.split("-")[1]
 
         if code>=codeStart and code<=codeEnd:
             outputList.append(icd)
         else:
-            icdFirstLevel = Icd10Cm(icd.code, icd.name, None, None)
+            icdFirstLevel = Icd10Cm(icd.id, icd.name, ["Loading.."])
             outputList.append(icdFirstLevel)
     return outputList
 
